bb2228381f49e8704f042ea408d81c346e1979b4
xyvie lyons <xyvielyons@gmail.com>
2025-01-06 01:13:42 +0300

    basic setup of application

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'uploadthing.com',
+      'utfs.io',
+      'img.clerk.com',
+      'subdomain',
+      'files.stripe.com',
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
